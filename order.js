20c85dfeed616d7ce3b8e092d8fbcd3175d08f8c
hannahshader <38895141+hannahshader@users.noreply.github.com>
2024-02-16 14:30:26 -0500

    Joe's Hot Dogs

---
+++ order.js
@@ -0,0 +1,52 @@
+/**
+ * @Author: Hannah Shader
+ * @Date:   2024-02-15 01:03:26
+ * @Last Modified by:   Hannah Shader
+ * @Last Modified time: 2024-02-16 14:29:24
+ */
+
+const PRICE_HOTDOG = 4.65;
+const PRICE_FRIES = 3.75;
+const PRICE_SODA = 1.89;
+
+function showMoney(amount) {
+    let rounded = Math.floor(amount * 100) / 100;
+    let difference = amount - rounded;
+    if (difference >= 0.005) {
+        rounded += 0.01;
+    }
+    return rounded.toFixed(2);
+}
+
+
+function takeOrder() {
+    // Any non-integer value will be intepreted as a quanitity of 0
+    // Negative numbers will be interpreted as positive numbers
+    const numDogs = Math.abs(parseInt(prompt("Welcome to our order system!\n\nPlease enter the quantity for each item you want to order. Non-numeric answers and cancels default to a quantity of 0!\n\nFirst, how many hotdog orders would you like?"), 10) || 0);
+    const numFries = Math.abs(parseInt(prompt("Great! Next, how many orders of fries would you like?"), 10) || 0);
+    const numSoda = Math.abs(parseInt(prompt("Awesome. How many sodas would you like?"), 10) || 0);
+
+    let subtotal_before_discount = (numDogs * PRICE_HOTDOG) + (numFries * PRICE_FRIES) + (numSoda * PRICE_SODA);
+    
+    let discount = subtotal_before_discount >= 25 ? subtotal_before_discount * 0.1 : 0;
+    subtotal_after_discount = subtotal_before_discount - discount;
+    
+    const tax = subtotal_after_discount * 0.0625;
+    const total = subtotal_after_discount + tax;
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        document.getElementById("takeOrderButton").addEventListener("click", takeOrder);
+    });
+
+    document.getElementById('orderSummary').style.display = 'block';
+    
+    document.getElementById('orderSummary').innerHTML = `
+        <p>Hotdogs Ordered (${numDogs}): $${showMoney(numDogs * PRICE_HOTDOG)}</p>
+        <p>Fries Ordered (${numFries}): $${showMoney(numFries * PRICE_FRIES)}</p>
+        <p>Sodas Ordered (${numSoda}): $${showMoney(numSoda * PRICE_SODA)}</p>
+        <p>Subtotal before discount: $${showMoney(subtotal_before_discount)}</p>
+        <p>Discount: $${showMoney(discount)}</p>
+        <p>Subtotal after discount: $${showMoney(subtotal_after_discount)}</p>
+        <p>Tax: $${showMoney(tax)}</p>
+        <p>Total: $${showMoney(total)}</p>`;
+}
